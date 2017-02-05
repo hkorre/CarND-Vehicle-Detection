a5bb379d32156dfb7a48466ac1c165d450f733c9
Hasan Korre <hkorre@gmail.com>
2017-02-05 14:52:38 -0800

    hkorre: adding stuff from old repo

---
+++ get_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get install unzip
+
+# vehicles labeled data
+curl -LOk https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/vehicles.zip
+unzip vehicles.zip
+
+# non-vehicle labeled data
+curl -LOk https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/non-vehicles.zip
+unzip non-vehicles.zip
+cp -r false_positives non-vehicles/
